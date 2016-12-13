5fc15156b37052b1d90b413aa73c2ced0bd7447b
poojab26 <pbhaumik26@gmail.com>
2016-12-14 00:52:50 +0530

    Initial commit

---
+++ app/src/main/java/poojab26/tastycoding/MainActivity.java
@@ -0,0 +1,62 @@
+package poojab26.tastycoding;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextSwitcher textSwitcher;
+    LinearLayout layout;
+
+    String textSwitcherText[] = {"TextSwitcher Example", "Next Text in TextSwitcher", "Android TextSwitcher", "TextSwitcher Tutorial", "TextSwitcher in Android"};
+
+    int switcherText = textSwitcherText.length;
+    int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textSwitcher = (TextSwitcher) findViewById(R.id.textSwitcher);
+        layout = (LinearLayout) findViewById(R.id.layout);
+
+        textSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                TextView switcherTextView = new TextView(getApplicationContext());
+                switcherTextView.setTextSize(24);
+                switcherTextView.setTextColor(Color.BLACK);
+                switcherTextView.setText("Click The Below Button");
+                return switcherTextView;
+            }
+        });
+
+        Animation animationOut = AnimationUtils.loadAnimation(this, android.R.anim.fade_out);
+        Animation animationIn = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
+
+        textSwitcher.setOutAnimation(animationOut);
+        textSwitcher.setInAnimation(animationIn);
+    }
+
+    public void nextText(View view) {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                textSwitcher.setText(textSwitcherText[counter]);
+                counter++;
+            }
+        }, 100);
+    }
+}
+
