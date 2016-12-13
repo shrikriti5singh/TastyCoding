94534d97b6824e86e827dff16bbf086bf6e5ec99
poojab26 <pbhaumik26@gmail.com>
2016-12-14 02:33:47 +0530

    added videoviewer

--- app/src/main/java/poojab26/tastycoding/MainActivity.java
+++ app/src/main/java/poojab26/tastycoding/MainActivity.java
@@ -1,74 +1,47 @@
 package poojab26.tastycoding;
 
-import android.content.Intent;
-import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.LinearLayout;
+import android.widget.MediaController;
 import android.widget.TextSwitcher;
-import android.widget.TextView;
-import android.widget.ViewSwitcher;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextSwitcher textSwitcher;
-    LinearLayout layout;
-
-    String textSwitcherText[] = {"TextSwitcher Example", "Next Text in TextSwitcher", "Android TextSwitcher", "TextSwitcher Tutorial", "TextSwitcher in Android"};
-
-    int switcherText = textSwitcherText.length;
+    VideoView videoView;
     int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        playAudio();
 
         textSwitcher = (TextSwitcher) findViewById(R.id.textSwitcher);
-        layout = (LinearLayout) findViewById(R.id.layout);
+        videoView =(VideoView)findViewById(R.id.videoView1);
 
-        textSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
-            @Override
-            public View makeView() {
-                TextView switcherTextView = new TextView(getApplicationContext());
-                switcherTextView.setTextSize(24);
-                switcherTextView.setTextColor(Color.BLACK);
-                switcherTextView.setText("Click The Below Button");
-                return switcherTextView;
-            }
-        });
+        playVideo();
 
-        Animation animationOut = AnimationUtils.loadAnimation(this, android.R.anim.fade_out);
-        Animation animationIn = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
-
-        textSwitcher.setOutAnimation(animationOut);
-        textSwitcher.setInAnimation(animationIn);
     }
 
-    public void nextText(View view) {
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                textSwitcher.setText(textSwitcherText[counter]);
-                counter++;
-                stopAudio();
-            }
-        }, 100);
+    public void playVideo()
+    {
+        //Creating MediaController
+        MediaController mediaController= new MediaController(this);
+        mediaController.setAnchorView(videoView);
+
+        //specify the location of media file
+        Uri uri=Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.h);
+
+        //Setting MediaController and URI, then starting the videoView
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+        videoView.start();
     }
-    public void playAudio() {
-        Intent objIntent = new Intent(this, PlayAudio.class);
-        startService(objIntent);
     }
 
-    public void stopAudio() {
-        Intent objIntent = new Intent(this, PlayAudio.class);
-        stopService(objIntent);
-    }
-}
+
+
 
