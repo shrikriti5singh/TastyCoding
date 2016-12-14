ef13f6eb9de8cbc8b3f84ce3c2e055eaf7204417
shrikriti5singh <shri5kriti.singh@gmail.com>
2016-12-14 16:33:52 +0530

    I did more lessons.

--- app/src/main/java/poojab26/tastycoding/MainActivity.java
+++ app/src/main/java/poojab26/tastycoding/MainActivity.java
@@ -1,14 +1,16 @@
 package poojab26.tastycoding;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnlesson1;
+    Button btnlesson1,btnlesson2,btnlesson3,btnlesson4,btnlesson5,btnlesson6,btnlesson7;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +21,26 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, RecipeLessons.class);
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                prefs.edit().putString("lesson", "1").commit();
                 startActivity(intent);
             }
         });
+        btnlesson2=(Button)findViewById(R.id.less2);
+        btnlesson2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RecipeLessons.class);
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                prefs.edit().putString("lesson", "2").commit();
+                startActivity(intent);
+            }
+        });
+        btnlesson3=(Button)findViewById(R.id.less3);
+        btnlesson4=(Button)findViewById(R.id.less4);
+        btnlesson5=(Button)findViewById(R.id.less5);
+        btnlesson6=(Button)findViewById(R.id.less6);
+        btnlesson7=(Button)findViewById(R.id.less7);
     }
     }
 
